b473f42127ab02ba9cccf0f00acaedba4c93d531
Meng Xu <mengxu@fb.com>
2020-10-16 20:09:00 -0400

    fun with graph algorithms

--- language/tools/move-symexec/src/sym_exec_graph.rs
+++ language/tools/move-symexec/src/sym_exec_graph.rs
@@ -7,18 +7,67 @@ use petgraph::{
     algo::tarjan_scc,
     graph::{Graph, NodeIndex},
 };
-use std::collections::HashMap;
+use std::{
+    collections::{HashMap, HashSet},
+    iter::FromIterator,
+};
 
 use bytecode_verifier::control_flow_graph::{BlockId, ControlFlowGraph, VMControlFlowGraph};
+use move_core_types::{
+    identifier::{IdentStr, Identifier},
+    language_storage::ModuleId,
+};
 use vm::{
     access::{ModuleAccess, ScriptAccess},
-    file_format::{CompiledModule, CompiledScript},
+    file_format::{Bytecode, CompiledModule, CompiledScript},
 };
 
 use crate::sym_setup::SymSetup;
 
-/// An extended control-flow graph that encloses both script and module
-/// CFGs, i.e., a super graph that has CFGs connected by the call graph.
+/// The following classes aim for building an extended control-flow
+/// graph that encloses both script and module CFGs, i.e., a super
+/// graph that has CFGs connected by the call graph.
+
+/// This is the basic block (i.e., the node) in the super-CFG.
+struct ExecBlock {
+    module_id: Option<ModuleId>,
+    function_id: Identifier,
+    instructions: Vec<Bytecode>,
+}
+
+impl ExecBlock {
+    pub fn new(module_id: Option<ModuleId>, function_id: Identifier) -> Self {
+        Self {
+            module_id,
+            function_id,
+            instructions: vec![],
+        }
+    }
+
+    pub fn add_instruction(&mut self, bytecode: Bytecode) {
+        self.instructions.push(bytecode);
+    }
+}
+
+/// This is the control transition (i.e., the edge) in the super-CFG.
+enum ExecTransition {
+    /// Fall through: the next instruction is PC + 1
+    Fallthrough,
+
+    /// Conditional or unconditional (if condition is None) branch
+    Branch {
+        bytecode: Bytecode,
+        condition: Option<bool>,
+    },
+
+    /// Function call
+    Call { bytecode: Bytecode },
+
+    /// Function return
+    Ret,
+}
+
+/// This is the super-CFG graph representation.
 pub(crate) struct ExecGraph {}
 
 impl ExecGraph {
@@ -27,10 +76,16 @@ impl ExecGraph {
         script: &CompiledScript,
         tracked_modules: &[CompiledModule],
     ) -> Self {
+        // prepare
+        // let mut call_stack = vec![];
+        // let mut block_stack = vec![];
+
+        // build CFG for the script
         let script_code = script.code();
         let script_cfg = VMControlFlowGraph::new(&script_code.code);
-        cfg_to_dag(&script_cfg);
 
+        // TODO: to be removed
+        cfg_topological_sort(&script_cfg);
         for module in tracked_modules {
             let module_id = module.self_id();
             for func_def in module.function_defs() {
@@ -41,7 +96,7 @@ impl ExecGraph {
                 }
                 if let Some(code_unit) = &func_def.code {
                     let function_cfg = VMControlFlowGraph::new(&code_unit.code);
-                    cfg_to_dag(&function_cfg);
+                    cfg_topological_sort(&function_cfg);
                 }
             }
         }
@@ -51,35 +106,83 @@ impl ExecGraph {
     }
 }
 
-fn cfg_to_generic_graph(cfg: &VMControlFlowGraph) -> Graph<BlockId, ()> {
+// CFG / graph utils
+
+/// Convert a (partial) CFG into generic graphs so we can benefit
+/// from the algorithms, in particular, the SCC calculation algorithm,
+/// in the petgraph crate.
+fn partial_cfg_to_generic_graph(
+    cfg: &VMControlFlowGraph,
+    blockset: &HashSet<BlockId>,
+) -> Graph<BlockId, ()> {
     // convert a CFG into a generic graph provided by petgraph
     let mut graph = Graph::new();
 
     // add nodes
-    let node_map: HashMap<BlockId, NodeIndex> = cfg
-        .blocks()
-        .into_iter()
-        .map(|block_id| (block_id, graph.add_node(block_id)))
+    let node_map: HashMap<BlockId, NodeIndex> = blockset
+        .iter()
+        .map(|block_id| (*block_id, graph.add_node(*block_id)))
         .collect();
 
     // add edges
-    cfg.blocks().iter().for_each(|block_id| {
-        cfg.successors(*block_id).iter().for_each(|successor_id| {
-            // all nodes added before, we can safely unwrap here
-            graph.add_edge(
-                *node_map.get(block_id).unwrap(),
-                *node_map.get(successor_id).unwrap(),
-                (),
-            );
-        })
-    });
+    cfg.blocks()
+        .iter()
+        .filter(|block_id| blockset.contains(block_id))
+        .for_each(|block_id| {
+            cfg.successors(*block_id)
+                .iter()
+                .filter(|successor_id| blockset.contains(successor_id))
+                .for_each(|successor_id| {
+                    // all nodes added before, we can safely unwrap here
+                    graph.add_edge(
+                        *node_map.get(block_id).unwrap(),
+                        *node_map.get(successor_id).unwrap(),
+                        (),
+                    );
+                })
+        });
 
     // done
     graph
 }
 
-fn cfg_to_dag(cfg: &VMControlFlowGraph) {
-    // convert a CFG into DAG by abstracting loops into a single node
-    let graph = cfg_to_generic_graph(&cfg);
-    tarjan_scc(&graph).reverse();
+/// Iterate a (partial) CFG in DAG-topological order
+/// The function first convert the (partial) CFG into a DAG by
+/// abstracting each loop into a single component. And then iterate
+/// over the DAG in topological order. For each component seen in
+/// DAG-iteration, convert loop component into a sub DAG and further
+/// iterate that sub-DAG.
+fn partial_cfg_topological_sort(
+    cfg: &VMControlFlowGraph,
+    blockset: &HashSet<BlockId>,
+) -> Vec<BlockId> {
+    let mut sorted_blocks = vec![];
+
+    // partial CFG convertion
+    let graph = partial_cfg_to_generic_graph(&cfg, blockset);
+
+    // iterate in topological order.
+    // `petgraph::algo::tarjan_scc` guarantees reverse topological order
+    for component in tarjan_scc(&graph).into_iter().rev() {
+        if component.len() == 1 {
+            // this is a single block
+            sorted_blocks.push(*graph.node_weight(*component.last().unwrap()).unwrap());
+        } else {
+            // this is a loop, sort its blocks and concat the results
+            let loop_blockset = HashSet::from_iter(
+                component
+                    .iter()
+                    .map(|node_idx| *graph.node_weight(*node_idx).unwrap()),
+            );
+            sorted_blocks.extend(partial_cfg_topological_sort(cfg, &loop_blockset));
+        }
+    }
+
+    // done
+    sorted_blocks
+}
+
+fn cfg_topological_sort(cfg: &VMControlFlowGraph) -> Vec<BlockId> {
+    let blockset = HashSet::from_iter(cfg.blocks());
+    partial_cfg_topological_sort(cfg, &blockset)
 }
