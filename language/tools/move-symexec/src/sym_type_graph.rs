1e4af20ffa595a0a84b4ffac5a37fefcb0a5c2d6
Meng Xu <mengxu@fb.com>
2020-11-05 13:00:17 -0500

    build the type graph

--- language/tools/move-symexec/src/sym_type_graph.rs
+++ language/tools/move-symexec/src/sym_type_graph.rs
@@ -16,6 +16,61 @@ pub(crate) struct TypeGraph {
 }
 
 impl TypeGraph {
+    fn link(
+        variant_name: &str,
+        variant_type_arg: &ExecTypeArg,
+        involved_structs: &HashMap<StructContext, HashMap<Vec<ExecTypeArg>, String>>,
+        graph_node_map: &HashMap<String, NodeIndex>,
+        graph: &mut Graph<String, ()>,
+    ) {
+        match variant_type_arg {
+            ExecTypeArg::Bool
+            | ExecTypeArg::U8
+            | ExecTypeArg::U64
+            | ExecTypeArg::U128
+            | ExecTypeArg::Address
+            | ExecTypeArg::Signer => {}
+            ExecTypeArg::Vector { element_type } => {
+                TypeGraph::link(
+                    variant_name,
+                    element_type.as_ref(),
+                    involved_structs,
+                    graph_node_map,
+                    graph,
+                );
+            }
+            ExecTypeArg::Struct { context, type_args } => {
+                let dep_name = involved_structs
+                    .get(context)
+                    .unwrap()
+                    .get(type_args)
+                    .unwrap();
+                let dep_node_id = graph_node_map.get(dep_name).unwrap();
+                let src_node_id = graph_node_map.get(variant_name).unwrap();
+                graph.add_edge(*src_node_id, *dep_node_id, ());
+            }
+            ExecTypeArg::Reference { referred_type }
+            | ExecTypeArg::MutableReference { referred_type } => {
+                TypeGraph::link(
+                    variant_name,
+                    referred_type.as_ref(),
+                    involved_structs,
+                    graph_node_map,
+                    graph,
+                );
+            }
+            ExecTypeArg::TypeParameter { actual_type, .. } => {
+                TypeGraph::link(
+                    variant_name,
+                    actual_type.as_ref(),
+                    involved_structs,
+                    graph_node_map,
+                    graph,
+                );
+            }
+        }
+    }
+
     pub fn new(
         involved_structs: HashMap<StructContext, HashMap<Vec<ExecTypeArg>, String>>,
         analyzed_structs: HashMap<String, ExecStructInfo>,
@@ -24,16 +79,27 @@ impl TypeGraph {
 
         // create nodes
         let mut node_map: HashMap<String, NodeIndex> = HashMap::new();
-        for struct_variants in involved_structs.values() {
-            for variant_name in struct_variants.values() {
-                let exists =
-                    node_map.insert(variant_name.clone(), graph.add_node(variant_name.clone()));
-                debug_assert!(exists.is_none());
-            }
+        for variant_name in analyzed_structs.keys() {
+            node_map.insert(variant_name.clone(), graph.add_node(variant_name.clone()));
         }
 
-        // TODO: experimental
-        println!("Number of structs: {}", analyzed_structs.len());
+        // create edges
+        for (variant_name, variant_info) in analyzed_structs.iter() {
+            match variant_info {
+                ExecStructInfo::Native => {}
+                ExecStructInfo::Declared { field_map, .. } => {
+                    for field_type in field_map.values() {
+                        TypeGraph::link(
+                            variant_name,
+                            field_type,
+                            &involved_structs,
+                            &node_map,
+                            &mut graph,
+                        );
+                    }
+                }
+            }
+        }
 
         // done
         Self { graph }
